ce198f9f2062f98ae21c2f4b9f96eef38d37ad81
Anton <anton.jefcoate@just-eat.com>
2014-07-24 15:02:16 +0100

    Add a super simple sample app...

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JustSaying.Messaging.MessageHandling;
+using JustSaying.Models;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = JustSaying.CreateMeABus.InRegion("eu-west-1")
+                .WithSnsMessagePublisher<SomeMessage>()
+                .WithSqsTopicSubscriber()
+                .IntoQueue("AntonsQueue")
+                .WithMessageHandler(new HandleSomeMessage());
+            
+            bus.StartListening();
+
+            Console.WriteLine("What would you like to say?");
+            bus.Publish(new SomeMessage(Console.ReadLine()));
+
+            Console.ReadKey();
+        }
+
+        class SomeMessage : Message
+        {
+            public SomeMessage(string info)
+            {
+                Info = info;
+            }
+
+            public string Info { get; private set; }
+        }
+
+        class HandleSomeMessage : IHandler<SomeMessage>
+        {
+            public bool Handle(SomeMessage message)
+            {
+                Console.WriteLine("You wrote: '{0}'", message.Info);
+                return true;
+            }
+        }
+    }
+}
